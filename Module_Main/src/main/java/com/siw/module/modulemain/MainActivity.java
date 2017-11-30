88d0c0a89eae179d4e33ecc504d3f2a4b6c520f4
tongsiw <tonsiw@163.com>
2017-11-30 17:31:40 +0800

    v1.0.0

---
+++ Module_Main/src/main/java/com/siw/module/modulemain/MainActivity.java
@@ -0,0 +1,13 @@
+package com.siw.module.modulemain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
